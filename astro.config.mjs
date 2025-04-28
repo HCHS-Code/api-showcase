367843becb947a3f6bb51c3795a7173bbee1aa4e
Matthew Chisolm <39521893+mchisolm0@users.noreply.github.com>
2025-04-28 16:33:07 -0500

    Add preview builds for PRs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: 'https://hchs-code.github.io',
-  base: '/api-showcase',
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  base: process.env.BASE_PATH || '/',
 });
